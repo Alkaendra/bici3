f4f9ae92a251cbcfc314de8b3dc5b91fb669bae0
Alkaendra <frarilor@gmail.com>
2016-04-29 16:24:22 -0400

    Cambiando production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,10 @@ Bicicletea::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -44,7 +44,7 @@ Bicicletea::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-
+config.serve_static_files = false
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
